3c7f4ce1140a0be0d6dcd9da691eedafde8c9e1c
Gboettge <gustav.boettge@outlook.com>
2025-03-31 15:46:16 +0200

    +

---
+++ Uppgift/Uppgift_4_echoClient/echo_client.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ #include <cstring>
+ #include <unistd.h>
+ #include <arpa/inet.h>
+ 
+ int main() {
+    int sockfd;
+    struct sockaddr_in server_addr;
+    // TODO: Create TCP socket
+    // TODO: Connect to server
+    // TODO: Send message to server
+    // TODO: Receive echo message
+    // TODO: Close connection
+    return 0;
+ }
