cca5266fa0b09488aebf5098a942e97d66732f6a
Gboettge <gustav.boettge@outlook.com>
2025-03-31 15:26:16 +0200

    +

---
+++ Uppgift/Uppgift_2_UDP_sensor/UDP_sensor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+int main() {
+   int sockfd;
+   struct sockaddr_in server_addr;
+   // TODO: Create UDP socket
+   // TODO: Fill in server information (IP, Port)
+
+   while (true) {
+       // TODO: Create message
+       // TODO: Send message to server
+       // TODO: Sleep for 1 second
+   }
+   // TODO: Close socket
+   return 0;
+}
