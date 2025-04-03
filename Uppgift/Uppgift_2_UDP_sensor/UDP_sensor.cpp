24e845e81273bf6b0780a4d5efcc8c1fc86b6719
Gboettge <gustav.boettge@outlook.com>
2025-04-03 11:30:19 +0200

    +

--- Uppgift/Uppgift_2_UDP_sensor/UDP_sensor.cpp
+++ Uppgift/Uppgift_2_UDP_sensor/UDP_sensor.cpp
@@ -2,18 +2,57 @@
 #include <cstring>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
-
+bool InitWinsockDLL()
+{
+  WSADATA wsaData;
+  ///* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
+  WORD wVersionRequested = MAKEWORD(2, 2);
+  int iResult = WSAStartup(wVersionRequested, &wsaData);
+ 
+  if (iResult != 0)
+  {
+    std::cout <<  "WSAStartup failed with error: "<< iResult << std::endl;
+    return false;
+  }
+ 
+  if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
+  {
+    /* Tell the user that we could not find a usable */
+    /* WinSock DLL.                                  */
+    std::cout <<  "Could not find a usable version of Winsock.dll" << std::endl;
+    WSACleanup();
+    return false;
+  }
+  return true;
+}
+ 
 int main() {
+   InitWinsockDLL();
    int sockfd;
    struct sockaddr_in server_addr;
    // TODO: Create UDP socket
+   sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+   if (sockfd < 0)
+   {
+       perror("Socket creation failed");
+       return 1;
+   }
+
    // TODO: Fill in server information (IP, Port)
+   server_addr.sin_family = AF_INET;
+   server_addr.sin_port = htons(5000);
+   server_addr.sin_addr.s_addr= inet_addr("172.16.217.216");
 
    while (true) {
        // TODO: Create message
+       const char* message = "Gustav";
        // TODO: Send message to server
+       std::cout << "Sending: " << message << std::endl;
+       sendto(sockfd, message, strlen(message), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
        // TODO: Sleep for 1 second
+       Sleep(1000);
    }
    // TODO: Close socket
+   closesocket(sockfd);
    return 0;
 }
--- Uppgift/Uppgift_3_UDP_server/UDP_server.cpp
+++ Uppgift/Uppgift_3_UDP_server/UDP_server.cpp
@@ -1,20 +1,45 @@
 #include <iostream>
 #include <cstring>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+
 int main() {
    int sockfd;
    struct sockaddr_in server_addr, client_addr;
    socklen_t addr_len = sizeof(client_addr);
    // TODO: Create UDP socket
+   sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+   if(sockfd = -1){
+    perror("Socket creation failed");
+    return 1;
+   }
    // TODO: Bind to a port
+   server_addr.sin_family = AF_INET;
+   server_addr.sin_addr.s_addr = INADDR_ANY;
+   server_addr.sin_port = htons(5000);
+
+   if(bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+    perror("bind failed");
+    closesocket(sockfd);
+    return 1;
+   }
+   std::cout << "Listening to port: " << server_addr.sin_port << std::endl; //kanske ändra sinport till string nummber
    while (true) {
        char buffer[1024];
        memset(buffer, 0, sizeof(buffer));
        // TODO: Receive message from client
+       int n = recvfrom(sockfd, buffer, sizeof(buffer), 0 ,(struct sockaddr*)&client_addr, &addr_len);
+
        // TODO: Print received message
+       if (n > 0 ){
+        //    char client_ip[INET_ADDRSTRLEN];
+        //    inet_ntoa(AF_I);
+           std::cout << buffer << std::endl; // ändra?
+       }
        // TODO (Optional): Send acknowledgment
    }
    // TODO: Close socket
+   closesocket(sockfd);
    return 0;
 }
